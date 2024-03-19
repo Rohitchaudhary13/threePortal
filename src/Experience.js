ea0ff24f28d0fb03ab95b4acabcf3178a9676fb5
Bloodsplash <abhinavchaudhary1318@gmail.com>
2024-03-19 18:15:19 +0530

    updated zooming

--- src/Experience.js
+++ src/Experience.js
@@ -1,57 +1,18 @@
 import { shaderMaterial, Sparkles, Center, useTexture, useGLTF, OrbitControls } from '@react-three/drei'
 import * as THREE from 'three'
 import { useFrame, extend } from '@react-three/fiber'
-import { useRef } from 'react'
-import portalVertexShader from './shaders/portal/vertex.js'
-import portalFragmentShader from './shaders/portal/fragment.js'
+import { useRef, useEffect } from 'react'
 
-// const PortalMaterial = shaderMaterial(
-//     {
-//         uTime: 0,
-//         uColorStart: new THREE.Color('#ffffff'),
-//         uColorEnd: new THREE.Color('#000000')
-//     },
-//     portalVertexShader,
-//     portalFragmentShader
-// )
 
-// extend({ PortalMaterial })
+
+extend({ OrbitControls })
 
 export default function Experience()
 {
-    // const { nodes } = useGLTF('./model/portal.glb')
-
-    // const bakedTexture = useTexture('./model/baked.jpg')
-    // bakedTexture.flipY = false
-    
-    // const portalMaterial = useRef()
-
-    // useFrame((state, delta) =>
-    // {
-    //     portalMaterial.current.uTime += delta
-    // })
-
     return <>
 
-        <OrbitControls makeDefault />
-
+        <OrbitControls enableZoom={false} />
         <Center>
-            {/* <mesh geometry={ nodes.baked.geometry }>
-                <meshBasicMaterial map={ bakedTexture } />
-            </mesh>
-
-            <mesh geometry={ nodes.poleLightA.geometry } position={ nodes.poleLightA.position }>
-                <meshBasicMaterial color="#ffffe5" />
-            </mesh>
-
-            <mesh geometry={ nodes.poleLightB.geometry } position={ nodes.poleLightB.position }>
-                <meshBasicMaterial color="#ffffe5" />
-            </mesh>
-
-            <mesh geometry={ nodes.portalLight.geometry } position={ nodes.portalLight.position } rotation={ nodes.portalLight.rotation }>
-                <portalMaterial ref={ portalMaterial } />
-            </mesh> */}
-
             <Sparkles
 		        size={ 6 }
                 scale={ [ 4, 2, 4 ] }

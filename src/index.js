e0f0d27f0eca8a718e7cc1a2b2d39a5d11bcf9db
Rohit Chaudhary <71605903+Rohitchaudhary13@users.noreply.github.com>
2024-05-01 20:32:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ root.render(
     <Canvas
         flat
         camera={ {
-            fov: 45,
+            fov: 75,
             near: 0.1,
             far: 50,
             position: [ 1, 2, 6 ]
